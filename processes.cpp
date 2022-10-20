d06cd8f899e762eb029ede74274d14f8c82a6450
Zach Clow <zachclow@uw.edu>
2022-10-20 14:15:23 -0700

    More debugging info

--- processes.cpp
+++ processes.cpp
@@ -104,10 +104,10 @@ int main(int argc, char *argv[])
             cerr << "grep reached\n";
             cerr << "argv[1]: " + (string) argv[1];
             cerr << "\n";
-            cerr << "cin: ";
-            std::string cont;
-            cin >> cont;
-            cerr << cont;
+            //cerr << "cin: ";
+            //std::string cont;
+            //cin >> cont;
+            //cerr << cont;
             execlp("grep", "grep", argv[1], (char *) NULL);
             cerr << "grep failed\n";
          }
